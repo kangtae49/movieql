eea3f053df8ddc0793b71930c85dfbd31de75278
김강태 <kangtae49@gmail.com>
2020-12-26 13:19:49 +0900

    add id

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,15 +1,18 @@
 const people = [
     {
+        id: "0",
         name: "Nicolas",
         age: 18,
         gender: "female"
     },
     {
+        id: "1",
         name: "홍길동",
         age: 18,
         gender: "female"
     },
     {
+        id: "2",
         name: "이순신",
         age: 18,
         gender: "female"
