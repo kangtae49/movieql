ba7ffdce841da71d6daedef17695d537b29d132c
김강태 <kangtae49@gmail.com>
2020-12-26 14:23:24 +0900

    add, delete

--- graphql/db.js
+++ graphql/db.js
@@ -1,30 +1,30 @@
 export let movies = [
     {
-        id: "0",
+        id: 0,
         name: "Nicolas",
         score: 18,
     },
     {
-        id: "1",
+        id: 1,
         name: "홍길동",
         score: 18,
     },
     {
-        id: "2",
+        id: 2,
         name: "이순신",
         score: 18,
     },
 ];
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
+    const filteredPeople = people.filter(person => person.id === id);
     return filteredPeople[0]
 };
 
 export const getMovies = () => movies;
 
 export const deleteMovie = id => {
-    const cleanMovies = movies.filter(movie => movie.id != String(id));
+    const cleanMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanMovies.length) {
         movies = cleanMovies;
         return true
@@ -32,3 +32,14 @@ export const deleteMovie = id => {
         return false;
     }
 };
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
+
