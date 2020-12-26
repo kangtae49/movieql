2aa95ca436e4c0970f3d997c68cdeff3d6ed4a93
김강태 <kangtae49@gmail.com>
2020-12-26 13:16:31 +0900

    get people

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,24 @@
-const nicolas = {
-    name: "Nicolas",
-    age: 18,
-    gender: "female"
-}
+const people = [
+    {
+        name: "Nicolas",
+        age: 18,
+        gender: "female"
+    },
+    {
+        name: "홍길동",
+        age: 18,
+        gender: "female"
+    },
+    {
+        name: "이순신",
+        age: 18,
+        gender: "female"
+    },
+];
 
 const resolvers = {
     Query: {
-        person: () => nicolas
+        people: () => people
     }
 };
 
