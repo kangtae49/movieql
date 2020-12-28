8bd201b9a49683a338bc685b630eed7b42004bc2
김강태 <kangtae49@gmail.com>
2020-12-28 19:47:36 +0900

    axios

--- graphql/db.js
+++ graphql/db.js
@@ -1,19 +1,45 @@
-import fetch from "node-fetch";
+import axios from "axios";
+const BASE_URL = "https://yts-proxy.now.sh/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-const API_URL = 'https://yts.am/api/v2/list_movies.json?'
-// '?limit=50&minimum_rating=9'
-
-export const getMovies = (limit=10, rating=8.5) => {
-    let REQUEST_URL = API_URL;
-    if(limit > 0) {
-        REQUEST_URL += `limit=${limit}`;
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
     }
-    if(rating > 0){
-        REQUEST_URL += `&minimum_rating=${rating}`;
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
     }
+  });
+  return movies;
+};
 
-    return fetch(REQUEST_URL)
-        .then(res => res.json())
-        .then(json => json.data.movies);
-}
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movie;
+};
 
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movies;
+};
